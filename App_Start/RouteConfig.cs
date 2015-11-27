fa41856b5e8e4a2ed672c42ea19286010a354bb4
juliusbacosa <juliusbacosa@gmail.com>
2015-11-27 11:02:35 +0800

    - controls, masterpage, repository, dbcontext

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -12,8 +12,15 @@ namespace SportsStore.App_Start
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.MapPageRoute(null, "list/{category}/{page}", "~/Pages/Listing.aspx");
+            routes.MapPageRoute(null, "list/{page}", "~/Pages/Listing.aspx");
             routes.MapPageRoute(null, "", "~/Pages/Listing.aspx");
             routes.MapPageRoute(null, "list", "~/Pages/Listing.aspx");
+
+            routes.MapPageRoute("cart", "cart", "~/Pages/CartView.aspx");
+            routes.MapPageRoute("checkout", "checkout", "~/Pages/Checkout.aspx");
+
+            routes.MapPageRoute("admin_orders", "admin/orders", "~/Pages/Admin/Orders.aspx");
+            routes.MapPageRoute("admin_products", "admin/products", "~/Pages/Admin/Products.aspx");
         }
     }
 }
